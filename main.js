3aeebd928fb41d9feec3866de2a4848012a51ba2
PedroRaduan <108097314+PedroRaduan@users.noreply.github.com>
2023-06-23 14:09:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const timer_btn = document.querySelector('#timer');
+const cronometro_btn = document.querySelector('#cronometro');
+
+
+timer_btn.addEventListener('click', function(){
+    window.location = 'timer.html';
+});
+
+cronometro_btn.addEventListener('click', function(){
+    window.location = 'cronometro.html';
+});
